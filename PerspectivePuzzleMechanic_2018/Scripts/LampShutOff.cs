493b3b14b2c694d97bc94c343b4b3f72a55c7acd
Daniel C Menezes <d.cavalcante.m@gmail.com>
2020-09-29 16:57:27 -0300

    Added version on unity 2018

---
+++ PerspectivePuzzleMechanic_2018/Scripts/LampShutOff.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampShutOff : MonoBehaviour {
+
+    private Transform light;
+
+	// Use this for initialization
+	void Start () {
+        light = this.gameObject.transform.GetChild(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(this.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            Destroy(light.gameObject);
+            this.GetComponent<LampShutOff>().enabled = false;
+        }
+	}
+}
---
+++ PerspectivePuzzleMechanic_2018/Scripts/SparksPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparksPlayer : MonoBehaviour {
+
+    private GameObject relatedObject;
+    public ParticleSystem sparkPS;
+    public GameObject sparkLight;
+
+    private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        position = sparkPS.transform.position;
+        relatedObject = this.transform.parent.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (relatedObject.layer == 8 && this != null)
+        {
+            sparkPS.transform.parent = null;
+            sparkPS.transform.position = position;
+            sparkPS.Play();
+            sparkLight.SetActive(true);
+            sparkPS.gameObject.layer = 0;
+            this.GetComponent<SparksPlayer>().enabled = false;
+        }
+
+	}
+}
